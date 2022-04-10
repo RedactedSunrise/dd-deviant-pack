1c5d1f93a6ef8641865f0b650a96f3e48cf94dc5
Nebby <nebby131999@gmail.com>
2022-04-09 21:58:03 -0400

    no more work til name is decided

---
+++ DDVariantPack/Assets/MainProject/MainClass.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HG.Reflection;
+using R2API.Utils;
+using System.Security;
+using System.Security.Permissions;
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+[module: UnverifiableCode]
+[assembly: SearchableAttribute.OptIn]
+
+namespace DDVariants
+{
+    [BepInDependency(Moonstorm.MoonstormSharedUtils.GUID)]
+    [NetworkCompatibility]
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class MainClass : BaseUnityPlugin
+    {
+        public const string GUID = "com.DDTeam.DDVariantPack";
+        public const string NAME = "DDVariantPack";
+        public const string VERSION = "0.0.1";
+
+        public static MainClass Instance;
+        public void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
